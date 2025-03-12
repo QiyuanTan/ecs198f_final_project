504fd47edb379e6d10cba9aacbb5aa0e8b5be773
Qiyuan Tan <119595547+QiyuanTan@users.noreply.github.com>
2025-03-11 21:16:42 -0700

    added a function of getting a piece at a given square, added some structure at play_move

--- pychess/logic/chess_logic.py
+++ pychess/logic/chess_logic.py
@@ -1,25 +1,25 @@
 class ChessLogic:
-    def __init__(self):
-        """
-        Initalize the ChessLogic Object. External fields are board and result
+	def __init__(self):
+		"""
+		Initalize the ChessLogic Object. External fields are board and result
 
-        board -> Two Dimensional List of string Representing the Current State of the Board
-            P, R, N, B, Q, K - White Pieces
+		board -> Two Dimensional List of string Representing the Current State of the Board
+			P, R, N, B, Q, K - White Pieces
 
-            p, r, n, b, q, k - Black Pieces
+			p, r, n, b, q, k - Black Pieces
 
-            '' - Empty Square
+			'' - Empty Square
 
-        result -> The current result of the game
-            w - White Win
+		result -> The current result of the game
+			w - White Win
 
-            b - Black Win
+			b - Black Win
 
-            d - Draw
+			d - Draw
 
-            '' - Game In Progress
-        """
-        self.board = [
+			'' - Game In Progress
+		"""
+		self.board = [
 			['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 			['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
 			['','','','','','','',''],
@@ -29,20 +29,68 @@ class ChessLogic:
 			['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
 			['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
 		]
-        self.result = "" 
+		self.result = ""
+		self.move = "w"
 
-    def play_move(self, move: str) -> str:
-        """
-        Function to make a move if it is a valid move. This function is called everytime a move in made on the board
+	def play_move(self, move: str) -> str:
+		"""
+		Function to make a move if it is a valid move. This function is called everytime a move in made on the board
 
-        Args:
-            move (str): The move which is proposed. The format is the following: starting_sqaure}{ending_square}
-            
-            i.e. e2e4 - This means that whatever piece is on E2 is moved to E4
+		Args:
+			move (str): The move which is proposed. The format is the following: starting_sqaure}{ending_square}
 
-        Returns:
-            str: Extended Chess Notation for the move, if valid. Empty str if the move is invalid
-        """
-        
-        #Implement this
-        pass
+			i.e. e2e4 - This means that whatever piece is on E2 is moved to E4
+
+		Returns:
+			str: Extended Chess Notation for the move, if valid. Empty str if the move is invalid
+		"""
+		starting = move[:2]
+		ending = move[2:]
+		starting_piece = self._get_piece(starting)
+		ending_piece = self._get_piece(ending)
+
+		# skip if the starting piece is invalid
+		if self._invalid_starting_piece(starting_piece):
+			return ""
+
+		# handle special moves
+		if self._is_castling(move):
+			return self._handle_castling(move)
+		elif self._is_en_passant(move):
+			return self._handle_en_passant(move)
+		elif self._is_promotion(move):
+			return self._handle_promotion(move)
+
+		return ""
+
+
+	def _get_piece(self, square:str) -> str:
+			"""
+			@brief: Function to get the piece at the given move
+			@param square: The square to get the piece from, e.g. e4
+			@return: The piece at the square
+			"""
+			col = ord(square[0]) - ord('a')
+			row = 8 - int(square[1])
+			return self.board[row][col]
+
+	def _invalid_starting_piece(self, starting_piece):
+		return starting_piece == '' or (self.move == 'w' and starting_piece.islower()) or (self.move == 'b' and starting_piece.isupper())
+
+	def _is_castling(self, move):
+		raise NotImplementedError
+
+	def _handle_castling(self, move):
+		raise NotImplementedError
+
+	def _is_en_passant(self, move):
+		raise NotImplementedError
+
+	def _handle_en_passant(self, move):
+		raise NotImplementedError
+
+	def _is_promotion(self, move):
+		raise NotImplementedError
+
+	def _handle_promotion(self, move):
+		raise NotImplementedError
--- pychess/test_chess_logic.py
+++ pychess/test_chess_logic.py
@@ -5,6 +5,25 @@ class TestCases(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
+class TestUtils(unittest.TestCase):
+    def test_get_piece(self):
+        from logic.chess_logic import ChessLogic
+        logic = ChessLogic()
+        logic.board = [
+            ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+        ]
+        self.assertEqual(logic._get_piece('a1'), 'R')
+        self.assertEqual(logic._get_piece('h8'), 'r')
+        self.assertEqual(logic._get_piece('e4'), '')
+        self.assertEqual(logic._get_piece('a2'), 'P')
+
 
 if __name__ == '__main__':
     unittest.main()
